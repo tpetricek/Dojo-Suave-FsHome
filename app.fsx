13f6badfe70e931dcbadbed038e0ea4659ec4cea
tpetricek <tomas@tomasp.net>
2015-05-14 00:06:02 +0100

    Add FCS for Heroku

--- app.fsx
+++ app.fsx
@@ -2,6 +2,7 @@
 // Start up Suave.io
 // --------------------------------------------------------------------------------------
 
+#r "packages/FSharp.Compiler.Service/lib/net45/FSharp.Compiler.Service.dll"
 #r "packages/FAKE/tools/FakeLib.dll"
 #r "packages/Suave/lib/net40/Suave.dll"
 
