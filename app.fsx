792fce76ed6bf3c00da53481d0285eb697a15853
tpetricek <tomas@tomasp.net>
2015-05-21 13:36:03 +0200

    Add more offline samples

--- app.fsx
+++ app.fsx
@@ -10,7 +10,7 @@ open Suave.Types
 open FSharp.Data
 
 // ----------------------------------------------------------------------------
-// Step #1: Introducing Suave
+// Introducing Suave
 // ----------------------------------------------------------------------------
 
 // In Suave 'WebPart' represents a (part of a) web server. The following creates
@@ -33,8 +33,9 @@ if false then
   cts.Cancel()
 
 
+
 // ----------------------------------------------------------------------------
-// Step #2: Routing using Suave combinators
+// Routing using Suave combinators
 // ----------------------------------------------------------------------------
 
 open Suave.Http.Successful
@@ -44,16 +45,25 @@ open Suave.Http.Applicatives
 let app_2 : WebPart =
   choose
     [ path "/" >>= OK "See <a href=\"/add/40/2\">40 + 2</a>"
-      pathScan "/add/%d/%d" (fun (a,b) -> OK((a + b).ToString()))
+      pathScan "/add/%d/%d" (fun (a,b) -> OK(string (a + b)))
       NOT_FOUND "Found no handlers" ]
 
+
+// TASK #1: FizzBuzz!
+// ------------------
+//
 // If you feel like the world needs one more FizzBuzz, then add a handler
 // that handles "/fizzbuzz/%d" and prints the result of fizz buzz for a
 // number %d together with a link to the next number!
+//
+//   fizzbuzz/1/  -->  1    <a href="../2/"> Next </a>
+//   fizzbuzz/2/  -->  2    <a href="../3/"> Next </a>
+//   fizzbuzz/3/  -->  Fizz <a href="../4/"> Next </a>
+
 
 
 // ----------------------------------------------------------------------------
-// Step #3: Doing more useful things!
+// Doing more useful things!  (!! INTERNET !!)
 // ----------------------------------------------------------------------------
 
 // The following snippet uses the JSON type provider to call OpenWeatherMap
@@ -67,12 +77,17 @@ if false then
   printfn "%A" london.Main.Temp
   printfn "http://openweathermap.org/img/w/%s.png" london.Weather.[0].Icon
 
+
+// TASK #2: Getting weather
+// ------------------------
+//
 // Create a web server that returns a page with the current weather
 // when we request: http://localhost:8083/weather/london
 
 
+
 // ----------------------------------------------------------------------------
-// Step #4: Understanding Suave WebParts and writing asynchronous servers
+// Understanding Suave WebParts and writing asynchronous servers
 // ----------------------------------------------------------------------------
 
 // Under the cover WebPart is a function 'Context -> Async<Context>'. We
@@ -89,23 +104,98 @@ let app_4 =
     [ path "/wait" >>= waitAndReturn
       NOT_FOUND "Found no handlers" ]
 
-// TODO: Improve the previous server so that it calls
-// `Weather.AsyncLoad` and avoids blocking
+// TASK #3: Async weather
+// ----------------------
+//
+// Improve the previous demo so that it calls `Weather.AsyncLoad`
+// and avoids blocking when getting the weather from the REST service
+
+
 
 // ----------------------------------------------------------------------------
-// Step #5: Writing F# home page web site using Suave.io
+// Processing RSS feeds in a web server
 // ----------------------------------------------------------------------------
 
+/// Helper function that returns nice HTML page with title & body
+let simplePage title body = 
+  File.ReadAllText(Path.Combine(__SOURCE_DIRECTORY__, "web/blank.html"))
+    .Replace("[TITLE]", title).Replace("[BODY]", body)
+
+
+// TASK #4: Reading RSS
+// --------------------
+//
+// Iterate over BBC news (or other RSS feed) and print the titles & descriptions
+// You can use 'BbcFeed.GetSample()' to load the sample XML file, or
+// you can use 'BbcFeed.Load(...)' to load data from a custom URL
+
+type BbcFeed = XmlProvider<"samples/bbc.xml">
+
+
+// TASK #5: News server
+// --------------------
+//
+// Implement WebPart that returns & nicely formats the data
+// For a given RSS news item, you should build something like
+//
+//  <li><h3>Title</h3> <p>Description</p> </li>
+//
+// The following gives you a template to fill & 'app_5' 
+// (below) is hosting this at 'http://localhos:8083/news'
+
+let getNews : WebPart = fun ctx -> async {
+  let news = 
+    [ for i in 1 .. 10 do
+        yield sprintf "<li>News item (#%d)</li>" i ]
+    |> String.concat ""
+  let body = sprintf "<ul>%s</ul>" news
+  let html = simplePage "BBC News" body
+  return! ctx |> Successful.OK(html) }
+
+
+// TASK #6: Add analytics
+// ----------------------
+//
+// Add handler for the '/words' path that loads the RSS feed and returns
+// a list with the most frequent words. To do this, you'll need to iterate
+// over the items, split descriptions into words and then calculate the
+// statistics using `Seq.countBy`. For example:
+
+[ "a"; "b"; "c"; "b" ]
+|> Seq.countBy (fun letter -> letter)
+|> Seq.sortBy (fun (letter, count) -> count)
+
+  
+/// The main handler for Suave server!
+let app_5 = 
+  choose [ 
+    path "/" >>= Successful.OK("""<html><body><ul>
+        <li>See <a href="/news">the latest news</a>!</li>
+        <li>See <a href="/words">most common words</a>!</li>
+      </ul></body></html>""")
+    path "/news" >>= getNews 
+    NOT_FOUND "Found no handlers" ]
+
+
+
+// ----------------------------------------------------------------------------
+// Writing F# home page web site using Suave.io (!! INTERNET !!)
+// ----------------------------------------------------------------------------
+
+// TASK #7: F# Homepage
+// --------------------
+//
 // Now, we want to write "F# homepage" that will show all the things
 // that matter on the internet - recent F# blogs, starred F# projects
 // on GitHub and recent comments and pull requests from the F# github org.
 // Here are some useful snippets: http://fssnip.net/r0
 
+
 // The following uses F# Data to define types that you'll need:
 
 type RssFeed = XmlProvider<"http://fpish.net/rss/blogs/tag/1/f~23">
-type GithubSearch = JsonProvider<"jsons/github-search.json">
-type GithubEvents = JsonProvider<"jsons/github-events.json">
+type GithubSearch = JsonProvider<"samples/github-search.json">
+type GithubEvents = JsonProvider<"samples/github-events.json">
 
 // To get the RSS feed or to call GitHub API, you need `Http.RequestString`
 // (the following sample shows `async` version of those, but feel free to
@@ -144,7 +234,7 @@ let template = Path.Combine(__SOURCE_DIRECTORY__, "web/index.html")
 let html = File.ReadAllText(template)
 
 /// The main handler for Suave server!
-let app_5 ctx = async {
+let app_6 ctx = async {
   let! data = [ getFeedNews() ] |> Async.Parallel
   let html =
     html.Replace("[FEED-NEWS]", data.[0])
@@ -157,4 +247,4 @@ let app_5 ctx = async {
 // called `app` - so define `app` to refer to the current step!
 // ----------------------------------------------------------------------------
 
-let app = app_1
+let app = app_5
